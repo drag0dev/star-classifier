0ca8f88d7d8e42a8634aa9b9c603c2bb1dc7269e
Dragoslav Tamindžija <dragodev@protonmail.com>
2024-05-08 20:07:15 +0200

    feat: scale numerical features

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+
+numerical_feature_cols = [
+    'Temperature (K)',
+    'Luminosity(L/Lo)',
+    'Radius(R/Ro)',
+    'Absolute magnitude(Mv)'
+]
 
 if __name__ == "__main__":
     data = pd.read_csv("data.csv")
@@ -13,9 +21,16 @@ if __name__ == "__main__":
     data['Spectral Class'] = label_encoder.fit_transform(data['Spectral Class'])
     data['Star color'] = label_encoder.fit_transform(data['Star color'])
 
+
+    # scale features
+    scaler = StandardScaler()
+    numerical_features = data[numerical_feature_cols]
+    scaler.fit(numerical_features)
+    data[numerical_feature_cols] = scaler.transform(numerical_features)
+
+    # prep data
     X = data.drop(['Star type'], axis=1)
     y = data['Star type']
-
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=420)
 
     knn = KNeighborsClassifier(n_neighbors=2)
@@ -25,5 +40,3 @@ if __name__ == "__main__":
 
     acc = accuracy_score(y_test, y_pred)
     print(acc)
-
-    pass
