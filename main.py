91d1be408ead75a305cd1630dfd8c6343a05a151
Dragoslav Tamindžija <dragodev@protonmail.com>
2024-05-08 19:55:08 +0200

    feat: impl basic knn

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
+import pandas as pd
+
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import LabelEncoder
+
 if __name__ == "__main__":
+    data = pd.read_csv("data.csv")
+
+    # encode Star Color and Spectral Class from string to int
+    label_encoder = LabelEncoder()
+    data['Spectral Class'] = label_encoder.fit_transform(data['Spectral Class'])
+    data['Star color'] = label_encoder.fit_transform(data['Star color'])
+
+    X = data.drop(['Star type'], axis=1)
+    y = data['Star type']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=420)
+
+    knn = KNeighborsClassifier(n_neighbors=2)
+    knn.fit(X_train, y_train)
+
+    y_pred = knn.predict(X_test)
+
+    acc = accuracy_score(y_test, y_pred)
+    print(acc)
+
     pass
